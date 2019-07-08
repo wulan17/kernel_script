c71e84e0cd34ef8e6cc9ebda5fdc3baefef72deb
iamsaalim <40296389+iamsaalim@users.noreply.github.com>
2019-07-08 18:38:45 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
+CONFIG=X00PD_defconfig
+CORES=$(grep -c ^processor /proc/cpuinfo)
+THREAD="-j$CORES"
+CROSS_COMPILE+="ccache "
+CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+echo -e "\n(i) Cloning toolcahins if folder not exist..."
+git clone https://github.com/raza231198/aarch64-linux-android-4.9
+# Export
+export ARCH=arm64
+export SUBARCH=arm64
+export PATH=/usr/lib/ccache:$PATH
+export CROSS_COMPILE
+export KBUILD_BUILD_USER=kherio
+export KBUILD_BUILD_HOST=electro_beta
+make  O=out $CONFIG $THREAD
