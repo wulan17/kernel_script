bcb51d3bc47e926e070aef57ef5af8b5fb7c9ee5
wulan17 <galihgustip@gmail.com>
2019-09-07 00:57:22 +0700

    fix #6
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- strip.sh
+++ strip.sh
@@ -1,7 +1,7 @@
 # Main Environment
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm/boot/zImage-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel
+ZIP_DIR=$HOME/build_kernel/AnyKernel
 CONFIG_DIR=$KERNEL_DIR/arch/arm/configs
 CONFIG=cactus_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
@@ -10,4 +10,5 @@ CROSS_COMPILE+="ccache "
 CROSS_COMPILE+="$PWD/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
 export zip_name="kernel-cactus-"$(env TZ='Asia/Jakarta' date +%Y%m%d)""
 cp $KERN_IMG $ZIP_DIR
+cd $ZIP_DIR
 zip -r $zip_name.zip ./*
