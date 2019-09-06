4d296405848e35c7ad12ac661af2e29b5f6ffb8e
wulan17 <galihgustip@gmail.com>
2019-09-07 01:37:59 +0700

    fix #10
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 $HOME/build_kernel/telegram -M "Build Start
-    Dev : wulan17
-    Product : Kernel
-    Device : #cactus
-    Branch : Oreo
-    Compiler : ""$(gcc --version)""
-    Compiler : ""$($PWD/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-gcc --version | head -n 1)""
-    Date : ""$(env TZ=Asia/Jakarta date)"""
+Dev : wulan17
+Product : Kernel
+Device : #cactus
+Branch : Oreo
+Compiler : ""$(gcc --version)""
+Compiler : ""$($HOME/kernel/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-gcc --version | head -n 1)""
+Date : ""$(env TZ=Asia/Jakarta date)"""
