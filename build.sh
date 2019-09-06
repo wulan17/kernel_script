441cf44370b3d7c86508337cdc2b6ad0e93f7878
wulan17 <galihgustip@gmail.com>
2019-09-06 22:53:03 +0700

    Cactus Changes
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 # Main Environment
 KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+KERN_IMG=$KERNEL_DIR/out/arch/arm/boot/zImage-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
-CONFIG=msm8937-perf_defconfig
+CONFIG_DIR=$KERNEL_DIR/arch/arm/configs
+CONFIG=cactus_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+CROSS_COMPILE+="$PWD/arm_arm-linux-androideabi-4.9/bin/arm_arm-linux-androideabi-"
 
 echo -e "\n(i) Cloning toolcahins if folder not exist..."
-git clone https://github.com/raza231198/aarch64-linux-android-4.9
+git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git arm_arm-linux-androideabi-4.9
 
 # Export
-export ARCH=arm64
-export SUBARCH=arm64
+export ARCH=arm
+export SUBARCH=arm
 export PATH=/usr/lib/ccache:$PATH
 export CROSS_COMPILE
-export KBUILD_BUILD_USER=kherio
-export KBUILD_BUILD_HOST=stormbreaker
+export KBUILD_BUILD_USER=wulan17
+export KBUILD_BUILD_HOST=Travis-CI
 make  O=out $CONFIG $THREAD
--- strip.sh
+++ strip.sh
@@ -1,12 +1,12 @@
 # Main Environment
 KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+KERN_IMG=$KERNEL_DIR/out/arch/arm/boot/zImage-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
-CONFIG=X00PD_defconfig
+CONFIG_DIR=$KERNEL_DIR/arch/arm/configs
+CONFIG=cactus_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+CROSS_COMPILE+="$PWD/arm_arm-linux-androideabi-4.9/bin/arm_arm-linux-androideabi-"
 
 
